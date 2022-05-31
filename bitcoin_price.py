da9cc466f729478d425385e030ed5d1aae95e70d
Slava <slavaorlov1980@gmail.com>
2022-06-01 00:41:04 +1000

    Добавил ключ bpi в data from api, метод _get_rate

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,85 +1,94 @@
 import requests
-import btc_calculate
 from datetime import date
+
 
 class BitcoinPrice:
     url = ''
     data_from_api = {}
     cached_data = {}
     
+
     def __init__(self, url):
         self.url = url
         self.data_from_api = self._req_data()
 
+
     def _req_data(self):
         response = requests.get(self.url)
-        return  response.json()
+        return response.json()["bpi"]
+
 
     def raw_data(self):
         return self.data_from_api
 
-    def btc_calculate(self, bit_count, currency_data=["USD"]):
-        self.cached_data = self.bit_convert(bit_count, currency_data)
 
+    def btc_calculate(self, bit_count, currency_data=["USD"]):
+        self.cached_data = self._bit_convert(bit_count, currency_data)
         return self.cached_data
 
-    def bit_convert(self, bit_count, currency):
+# разделить на методы
+
+    def _bit_convert(self, bit_count, currency):
         bit_dict = {}
         for i in self.data_from_api:
             if i in currency:
-                rate = self.data_from_api[i]['rate'].replace(',', '')
-                bit_dict[i] =  {
-                    "result" : self._btc_calculate(bit_count, float(rate)),
-                    "btc_count" : bit_count,
-                }
-                print(bit_dict[i])
+                bit_dict[i] = {
+                                "result" : self._btc_calculate(bit_count, self._get_rate(i)),
+                                "btc_count" : bit_count
+                                }
         return bit_dict
     
+
+    def _get_rate(self, currency_counter):
+        rate = self.data_from_api[currency_counter]['rate'].replace(',', '')
+        return float(rate)
+
+
     def _btc_calculate(self, bit_count, rate ):
-        return bit_count * rate
+        return f"{(bit_count * rate):,.2f}"
+
 
     def _result(self, bit_count, currency, result):
-        print_res = f'[{date.today()}] {bit_count} битков в валюте {currency} = {result}'
+        output_string = f'[{date.today()}] {bit_count} битков в валюте {currency} = {result}'
+        return output_string
 
 
     def print_to_console(self):
-        for i in self.cached_data:
-            print_res = self._result(self.cached_data[i]["btc_count"], i, self.cached_data[i]["result"])
-            print(print_res)
+        with open("bit_log.txt", "r") as log:
+            for i in log:
+                print(i)
+        # for i in self.cached_data:
+        #     print_res = self._result(self.cached_data[i]["btc_count"], i, self.cached_data[i]["result"])
+        #     print(print_res)
 
-    # def print_convert(calc, bit_count):
-    #     for i in calc:
-            
-    #         print_res = f'[{date.today()}] {bit_count} битков в валюте {i} = {calc[i]}'
-    #         print(print_res)
-    #         bit_log = open('bit_log.txt', 'a')
-    #         bit_log.write(print_res + '\n')
-    #         bit_log.close()
 
+    def add_to_log(self):
+        with open("bit_log.txt", "a") as log:
+            for i in self.cached_data:
+                log.write(self._result(self.cached_data[i]["btc_count"], i, self.cached_data[i]["result"]) + "\n")
+ 
 
 if __name__ == '__main__':
+
     url = "https://api.coindesk.com/v1/bpi/currentprice.json"
     bit_count = 20
     currency = ("USD", "EUR")
 
     req = BitcoinPrice(url)
+
     # Сырые данные полученные из api 
     raw_data = req.raw_data()
     calc_data = req.btc_calculate(bit_count, currency)
+    req.add_to_log()
+
     bit_count = 40
     calc_data_2 = req.btc_calculate(bit_count,currency)
+    req.add_to_log()
 
-    # req.save_to_log()
     req.print_to_console()
 
-    # Блок данных 
-    # print(req.data_from_api)
-    # # Слой получения данных
-    # json_data = req.req_data(url)
-    # # Слой бизнес логики
-    # calc_result = req.bit_convert(bit_count, currency, json_data["bpi"])
-    # # Слой представления
-    # req.print_convert(calc_result, bit_count)
+    print(f"{calc_data = }")
+    print(f"{calc_data_2 = }")
 
 
 # ДЗ 
